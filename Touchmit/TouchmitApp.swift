9e93de71649da2af758febfab5c4042eb1e48f62
Chingis <Kekwaker@gmail.com>
2024-10-25 14:16:42 +0800

    Initial Commit

---
+++ Touchmit/TouchmitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchmitApp.swift
+//  Touchmit
+//
+//  Created by Qin Chingis on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchmitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
